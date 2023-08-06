69b441bd23e1a72f2870459c1532341a7c905d54
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-06 15:14:31 +0800

    feature: implement createNewPostTitle method in postAdapter

--- src/modules/posts/infrastructure/adapters/postAdapter.ts
+++ src/modules/posts/infrastructure/adapters/postAdapter.ts
@@ -10,6 +10,25 @@ class PostAdapter {
 		return posts;
 	}
 
+	public async createPostTitle({ title }: { title: string }): Promise<Post> {
+		const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_URL}/posts/write/`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						title: title,
+					}),
+				}
+			);
+		const data = await response.json();
+		const school: Post = data
+
+		return school
+	}
+
 	public async getPostContentByPostId({ id }: { id: number }): Promise<PostContent> {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/content/`);		
 		const postContent: PostContent = await res.json();
