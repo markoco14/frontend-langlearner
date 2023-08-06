e5460cb9a575ffb06832dfd4c3882cf3149146f9
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-06 12:07:53 +0800

    feature: create getPostContentByPostId() method in PostAdapter

--- src/modules/posts/infrastructure/adapters/postAdapter.ts
+++ src/modules/posts/infrastructure/adapters/postAdapter.ts
@@ -1,12 +1,20 @@
 import { Post } from "../../domain/entities/Post";
+import { PostContent } from "../../domain/entities/PostContent";
 
 class PostAdapter {
 
 	public async listPosts(): Promise<Post[]> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/`);		
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/`);		 
 		const posts: Post[] = await res.json();
 
 		return posts;
+	}
+
+	public async getPostContentByPostId({ id }: { id: number }): Promise<PostContent> {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/content/`);		
+		const postContent: PostContent = await res.json();
+
+		return postContent;
 	}
 
 	
