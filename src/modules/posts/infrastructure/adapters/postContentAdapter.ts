f5510906ced477d506ceb35e6ac4eae303c1079c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-09 10:27:58 +0800

    chore: remove line

--- src/modules/posts/infrastructure/adapters/postContentAdapter.ts
+++ src/modules/posts/infrastructure/adapters/postContentAdapter.ts
@@ -6,7 +6,6 @@ class PostContentAdapter {
 		// LEVEL NUMBER HARDCODED
 		// DEFAULT ASSUMES LEVEL 0 - UNLEVELLED CONTENT
 		// WILL ADD LEVELS LATER AND THEN ADD TO REQUEST
-		console.log('post id in adapter', postId)
 		const level = 0;
 		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${postId}/content/level/${level}/`);		 
 		const postContent: PostContent = await res.json();
