fc3035bb6f7c0ebda08dd37c1e182434606b6c05
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-12 10:09:19 +0800

    feature: update read page to render words from arrays instead of text string

--- src/pages/read/[post]/index.tsx
+++ src/pages/read/[post]/index.tsx
@@ -4,19 +4,9 @@ import { readPostAdapter } from "@/modules/posts/infrastructure/adapters/readPos
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 
-type ContentPinyinPair = {
-  word: string,
-  pinyin: string
-}
-
-type ReadContent = {
-  title: string,
-  content: ContentPinyinPair[],
-}
-
 
 export default function Home() {
-  const [post, setPost] = useState<ReadContent>();
+  const [post, setPost] = useState<ReadPost>();
   const [loading, setLoading] = useState<boolean>(false);
 	const router = useRouter()
 
@@ -25,36 +15,21 @@ export default function Home() {
       setLoading(true);
       if (router.query.post) {
         const postContent = await readPostAdapter.getPostContentAndPinyin({postId: Number(router.query.post)});
-        const contentArray = postContent.content.split('');
-        console.log(postContent)
-        console.log(contentArray)
-        const pinyinArray = postContent.pinyin.split(' ');
-        console.log(pinyinArray)
-        console.log('length of arrays', contentArray.length, pinyinArray.length)
-
-
-        const readContentArray: ContentPinyinPair[] = []
-        contentArray.forEach((word, index) => {
-          const data: ContentPinyinPair = {
-            word: word,
-            pinyin: pinyinArray[index]
-          }
-          readContentArray.push(data)
-        })
-
-        const readContent = {
-          title: postContent.title,
-          content: readContentArray
-        }
-        
-        console.log(readContent)
-        setPost(readContent);
+        setPost(postContent);
         setLoading(false);
       }
     }
 
     getData();
   }, [router.query.post])
+
+  function copyTextToClipboard(text: string) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Text successfully copied to clipboard!');
+    }).catch(function(err) {
+        console.error('Unable to copy text to clipboard: ', err);
+    });
+}
   return (
     <Layout>
       {loading && (
@@ -62,14 +37,15 @@ export default function Home() {
       )}
       {!loading && post && (
         <article className="max-w-[70ch] mx-auto">
-          <h1 className="text-5xl mb-4">{post?.title}</h1>
+          <h1 className="text-5xl mb-4">{post?.id}</h1>
           <div className="flex flex-wrap">
-
-            {post?.content?.map((contentPinyinPair: ContentPinyinPair, index: number) => (
-              <div key={index} className="flex flex-col whitespace-normal">
-                <span className="text-center text-3xl">{contentPinyinPair.word}</span>
-                <span className="text-center text-sm">{contentPinyinPair.pinyin}</span>
-              </div>
+            {post?.content.map((array: string[], arrayIndex: number) => (
+              <p key={arrayIndex}>
+                {array.map((word: string, wordIndex: number) => (
+                  <span key={wordIndex} className="hover:bg-yellow-200 text-3xl" onClick={() => copyTextToClipboard(word)}>{word}</span>
+                ))}
+              </p>
+        
             ))}
           </div>
         </article>
